5761110832b0e5f739d616581ef45a49b903f5b9
wylong1991\wylong <wylong1991@126.com>
2018-05-21 10:01:52 +0800

    添加

--- app/src/main/java/com/wyl/helloword/MainActivity.java
+++ app/src/main/java/com/wyl/helloword/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("注释", "onCreate: 哈哈哈哈哈");
+        System.out.print("哈哈哈");
     }
 }
